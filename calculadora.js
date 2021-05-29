e27fb25ccc596d927c9a3ad92e3a6019b6b67d58
PiaTcq <mertrebucq@gmail.com>
2021-05-29 02:06:08 -0300

    subi archivos calculadora

---
+++ calculadora.js
@@ -0,0 +1,15 @@
+const sumar=require("./sumar");
+let resultadoSuma=sumar(1,2);
+console.log(resultadoSuma);
+
+const restar=require("./restar");
+let resultadoResta=restar(3,1);
+console.log(resultadoResta);
+
+const multiplicar=require("./multiplicar");
+let resultadoMultiplicacion=multiplicar(2,0);
+console.log(resultadoMultiplicacion);
+
+const dividir=require("./dividir");
+let resultadoDivision=dividir(2,0);
+console.log(resultadoDivision);
